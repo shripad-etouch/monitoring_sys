3c1cdecb7a8e55b37e00fbce08c9dbf2d6113e4a
Shripad <sjoshi@etouch.net>
2013-10-09 12:02:15 +0530

    application commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MonitoringSys::Application.config.secret_token = '097eedc709e6815705cde13024185169059da50542851273aeac618c59bb3223a54034affc8ed58a968fc7ed4594fdf2e61dc4453efa2f0b56f7394bdf67bf21'
