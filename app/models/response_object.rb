4d95d8aa0e7b0af8d7a6c904032c12a3587fbe53
Shripad <sjoshi@etouch.net>
2013-10-09 13:20:21 +0530

    ResponseObject Rollback

--- app/models/response_object.rb
+++
@@ -1,3 +0,0 @@
-class ResponseObject < ActiveRecord::Base
-  attr_accessible :name
-end
--- db/migrate/20131009073720_create_response_objects.rb
+++
@@ -1,9 +0,0 @@
-class CreateResponseObjects < ActiveRecord::Migration
-  def change
-    create_table :response_objects do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end
